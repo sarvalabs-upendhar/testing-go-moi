397b6b3cfd728ae5858b63cd03d38b504b6504c3
Sai Prasanth Vuppala <96536302+sarvalabs-sai@users.noreply.github.com>
2023-05-15 16:35:44 +0530

    revamp: changed ECDSA signing and verification to use btcec/btcd (#283)

--- mudra/signature/ecdsa/secp256k1_test.go
+++ mudra/signature/ecdsa/secp256k1_test.go
@@ -2,41 +2,56 @@ package ecdsa
 
 import (
 	"encoding/hex"
-	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
+	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/sarvalabs/moichain/mudra/common"
 	"github.com/sarvalabs/moichain/mudra/kramaid"
+	"github.com/stretchr/testify/require"
 )
 
 var (
-	samplePrivateKey = []byte{
-		223, 251, 80, 42, 148, 23, 80, 3, 41, 56, 67, 29, 6, 222, 100,
-		235, 198, 118, 247, 22, 36, 154, 171, 94, 29, 237, 207, 78, 250, 162, 67, 14,
-	}
-	sampleMessage = []byte("Hello MOI user, this is test string being signed")
+	privateKeyInHex   = "d4f2f4e558d484d42e06593e72c50af87dbb9b1b2b2128f35e8c0b6c8f3e1582"
+	message           = []byte("Hello MOI user, this is the message being signed")
+	expectedSignature = "0146304402201b2f03875387cb1964d70d414bae4fc15fc9b244967d973676a" +
+		"7087e628e15bc02207ee005106a92fa003e877ee5d773ded75ac6317672fc3b27def321be3b18d7ca"
 )
 
 func TestECDSASignWithSecp256k1(t *testing.T) {
 	var s256 EcdsaSecp256k1Signature
 
+	privateKeyBytes, _ := hex.DecodeString(privateKeyInHex)
 	kid := kramaid.KramaID("bvby3pBVU5BEL2jBHJrH23GTb9qe8nL4XHqqKzZVbth7gBZ5c3." +
 		"16Uiu2HAmGZr9gyQ7fDdmdBsRL29EjxR81Y74TEPbemBkyKuk2Ufj")
 
-	err := s256.Sign(sampleMessage, samplePrivateKey, kid)
+	err := s256.Sign(message, privateKeyBytes, kid)
 	if err != nil {
 		t.Error("signing with secp256k1 key failed")
 	}
 
 	rawSig := common.MarshalSignature(common.Signature(s256))
 	rawSigInHex := hex.EncodeToString(rawSig)
-	assert.EqualValues(t,
-		"0146304402207bb29ab5609dd826c114aed914f72b6f5d4860637671dafe7"+
-			"47d519a190b7528022011a02462f8f66ff1645e7df7466e2323d50a224eaee35104c5082aa90c9a027e",
+
+	require.Equal(t,
+		expectedSignature,
 		rawSigInHex,
-		fmt.Sprintf("> Expected 0146304402207bb29ab5609dd826c114aed914f72b"+
-			"6f5d4860637671dafe747d519a190b7528022011a02462f8f66ff1645e7df7466e2323d50a224eaee35104c5082aa90c9a027e "+
-			"for ECDSA Secp256k1 signature but got %v", rawSigInHex))
+	)
+}
+
+func TestECDSAVerifyWithSecp256k1(t *testing.T) {
+	privateKeyBytes, _ := hex.DecodeString(privateKeyInHex)
+	_, publicKey := btcec.PrivKeyFromBytes(privateKeyBytes)
+
+	sigInHexBytes, err := hex.DecodeString(expectedSignature)
+	require.NoError(t, err)
+
+	sig, err := common.UnmarshalSignature(sigInHexBytes)
+	require.NoError(t, err)
+
+	s256 := EcdsaSecp256k1Signature(sig)
+
+	verificationBool, err := s256.Verify(message, publicKey.SerializeCompressed())
+	require.NoError(t, err)
+
+	require.Equal(t, true, verificationBool)
 }
