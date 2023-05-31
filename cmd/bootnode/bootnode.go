6d45f648c940f4c11022cc32399acd35d6c2a858
Sahith N <sahith@sarva.ai>
2023-05-31 23:31:05 +0530

    chore: add conn checks to bootnode (#325)
    * chore: add conn checks to bootnode
    
    * chore: fix nil datastore option in bootnode KDHT
    
    ---------
    
    Co-authored-by: Rahul Lenkala <rahul@sarva.ai>

--- cmd/bootnode/bootnode.go
+++ cmd/bootnode/bootnode.go
@@ -7,12 +7,16 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+	"time"
 
 	badger "github.com/ipfs/go-ds-badger"
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/routing"
 	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
 	"github.com/multiformats/go-multiaddr"
@@ -29,6 +33,7 @@ var (
 	minConnReq    int
 	maxConnReq    int
 	dataStorePath string
+	peers         string
 )
 
 func GetCommand() *cobra.Command {
@@ -50,58 +55,7 @@ func parseFlags(cmd *cobra.Command) {
 	cmd.PersistentFlags().IntVar(&minConnReq, "min-conn-req", 200, "max number of connections allowed")
 	cmd.PersistentFlags().IntVar(&maxConnReq, "max-conn-req", 400, "max number of connections allowed")
 	cmd.PersistentFlags().StringVar(&dataStorePath, "data-store-path", "", "path to store bootnode data")
-}
-
-func getPrivateKey(keyfile string) (crypto.PrivKey, error) {
-	// Declare a variable for the private key
-	var key crypto.PrivKey
-
-	// Check if the key file exists at the given path
-	if _, err := os.Stat(keyfile); err == nil {
-		// Keyfile already exists
-		log.Println(" Found an existing key")
-
-		// Read the keyfile into bytes data and check for errors
-		data, err := ioutil.ReadFile(keyfile)
-		if err != nil {
-			// Return the error
-			return nil, err
-		}
-
-		// Unmarshal the private key data into the key and check for errors
-		key, err = crypto.UnmarshalPrivateKey(data)
-		if err != nil {
-			// Return the error
-			return nil, err
-		}
-	} else {
-		// Keyfile does not exist
-		log.Println("Generating new key")
-
-		// Generate a new RSA keypair and check for errors
-		key, _, err = crypto.GenerateKeyPairWithReader(crypto.Secp256k1, 256, crand.Reader)
-		if err != nil {
-			// Return the error
-			return nil, err
-		}
-
-		// Marshal the private key and check for errors
-		data, err := crypto.MarshalPrivateKey(key)
-		if err != nil {
-			// Return the error
-			return nil, err
-		}
-
-		// Write the private key data to keyfile and check for errors
-		err = ioutil.WriteFile(keyfile, data, 0o600)
-		if err != nil {
-			// Return the error
-			return nil, err
-		}
-	}
-
-	// Return the key and a nil error
-	return key, nil
+	cmd.PersistentFlags().StringVar(&peers, "peers", "", "comma-separated list of peers to connect")
 }
 
 func runCommand(cmd *cobra.Command, args []string) {
@@ -122,21 +76,22 @@ func startBootNode() {
 		panic(err)
 	}
 
-	var dataStore *badger.Datastore
+	dhtOptions := make([]dht.Option, 0)
+
 	if dataStorePath != "" {
-		dataStore, err = badger.NewDatastore(dataStorePath, nil)
+		dataStore, err := badger.NewDatastore(dataStorePath, nil)
 		if err != nil {
 			panic(err)
 		}
+
+		dhtOptions = append(dhtOptions, dht.Datastore(dataStore))
 	}
 
+	dhtOptions = append(dhtOptions, dht.Mode(dht.ModeServer))
+	dhtOptions = append(dhtOptions, dht.ProtocolPrefix(common.MOIProtocolStream))
+
 	selfRouting := libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
-		dhtOpts := []dht.Option{
-			dht.Mode(dht.ModeServer),
-			dht.ProtocolPrefix(common.MOIProtocolStream),
-			dht.Datastore(dataStore),
-		}
-		Dht, err := dht.New(context.Background(), h, dhtOpts...)
+		Dht, err := dht.New(context.Background(), h, dhtOptions...)
 		if err != nil {
 			panic(err)
 		}
@@ -152,7 +107,7 @@ func startBootNode() {
 
 	// libp2p.New constructs a new libp2p Host.
 	// Other options can be added here.
-	host, err := libp2p.New(
+	p2pHost, err := libp2p.New(
 		libp2p.ListenAddrs(sourceMultiAddr),
 		libp2p.NATPortMap(),
 		libp2p.ForceReachabilityPublic(),
@@ -165,8 +120,37 @@ func startBootNode() {
 		panic(err)
 	}
 
+	peerAddresses := strings.Split(peers, ",")
+	peerInfoList := make([]peer.AddrInfo, 0, len(peerAddresses))
+
+	for _, addr := range peerAddresses {
+		maddr, err := multiaddr.NewMultiaddr(addr)
+		if err != nil {
+			log.Printf("Invalid peer address: %s", err)
+
+			continue
+		}
+
+		peerinfo, err := peer.AddrInfoFromP2pAddr(maddr)
+		if err != nil {
+			log.Printf("Failed to parse peer address: %s", err)
+
+			continue
+		}
+
+		err = p2pHost.Connect(context.Background(), *peerinfo)
+		if err != nil {
+			log.Printf("Failed to connect to peer %s: %s", peerinfo.ID, err)
+		}
+
+		peerInfoList = append(peerInfoList, *peerinfo)
+	}
+
+	// Start a background task to maintain connections
+	go maintainConnections(context.Background(), p2pHost, peerInfoList)
+
 	fmt.Println("")
-	fmt.Printf("[*] Your Bootstrap ID Is: /ip4/%s/tcp/%v/p2p/%s\n", ipAddress, portNumber, host.ID().String())
+	fmt.Printf("[*] Your Bootstrap ID Is: /ip4/%s/tcp/%v/p2p/%s\n", ipAddress, portNumber, p2pHost.ID().String())
 	fmt.Println("")
 
 	var input string
@@ -182,11 +166,86 @@ func startBootNode() {
 
 		if input == "1" {
 			for _, v := range KadDHT.RoutingTable().ListPeers() {
-				protocols, _ := host.Peerstore().GetProtocols(v)
+				protocols, _ := p2pHost.Peerstore().GetProtocols(v)
 				fmt.Println("Peer ID", v, "Supported-Protocols", protocols)
-				fmt.Println("Peer Info", host.Peerstore().PeerInfo(v))
+				fmt.Println("Peer Info", p2pHost.Peerstore().PeerInfo(v))
 			}
 		} else if input == "2" {
+			return
+		}
+	}
+}
+
+func getPrivateKey(keyfile string) (crypto.PrivKey, error) {
+	key, err := loadExistingKey(keyfile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			key, err = generateAndStoreNewKey(keyfile)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return key, nil
+}
+
+func loadExistingKey(keyfile string) (crypto.PrivKey, error) {
+	data, err := ioutil.ReadFile(keyfile)
+	if err != nil {
+		return nil, err
+	}
+
+	key, err := crypto.UnmarshalPrivateKey(data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal private key: %w", err)
+	}
+
+	log.Println("Found an existing key")
+
+	return key, nil
+}
+
+func generateAndStoreNewKey(keyfile string) (crypto.PrivKey, error) {
+	key, _, err := crypto.GenerateKeyPairWithReader(crypto.Secp256k1, 256, crand.Reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate new key: %w", err)
+	}
+
+	data, err := crypto.MarshalPrivateKey(key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal private key: %w", err)
+	}
+
+	if err := ioutil.WriteFile(keyfile, data, 0o600); err != nil {
+		return nil, fmt.Errorf("failed to write key to file: %w", err)
+	}
+
+	log.Println("Generated new key")
+
+	return key, nil
+}
+
+func maintainConnections(ctx context.Context, host host.Host, peers []peer.AddrInfo) {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			for _, p := range peers {
+				if host.Network().Connectedness(p.ID) != network.Connected {
+					fmt.Printf("Reconnecting to peer %s", p.ID.String())
+
+					err := host.Connect(ctx, p)
+					if err != nil {
+						fmt.Printf("Failed to connect to peer %s: %s\n", p.ID.String(), err)
+					}
+				}
+			}
+
+		case <-ctx.Done():
 			return
 		}
 	}
